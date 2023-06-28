ea397b51cb8542603c28c69a45bbee713ff29f57
Victor S. Miller <victorsmiller@gmail.com>
2023-06-28 14:30:51 -0700

    write_csv and write_dimacs to output.py

--- cosets/__init__.py
+++ cosets/__init__.py
@@ -1,9 +1,11 @@
-from .graphs import dn_graph, write_csv
+from .graphs import dn_graph
+from .output import write_csv, write_dimacs
 from .connection import dn_neighbors, dn_counting
 from .maxsat import maxsat_mis, breaking_mis
 
 __all__ = ['dn_graph',
            'write_csv',
+           'write_dimacs',
            'dn_neighbors',
            'dn_counting',
            'maxsat_mis',
--- cosets/graphs.py
+++ cosets/graphs.py
@@ -35,35 +35,3 @@ def independent(num: int) -> nx.Graph:
     for elt in range(num):
         gph.add_node(elt)
     return gph
-
-def normalize(elt: Any) -> str:
-    """ Normalize list and tuple """
-    if isinstance(elt, (tuple, list)):
-        return '|'.join(map(str, elt))
-    else:
-        return str(elt)
-
-def generate_csv(gph: nx.Graph) -> Iterable[str]:
-    """ Generate the lines for a undirected graph """
-    # Find isolated nodes
-    # for node in gph.nodes:
-    #     if gph.degree(node) == 0:
-    #         yield normalize(node)
-    for edge in gph.edges:
-        yield normalize(edge[0]) + ',' + normalize(edge[1])
-
-def write_csv(gph: nx.Graph, name: str):
-    """
-    Write a csv file for a graph.
-    """
-    with open(name, 'w', encoding='utf8') as fil:
-        fil.write('\n'.join(generate_csv(gph)))
-        fil.write('\n')
-
-def write_independent(num: int, direct: str):
-    """
-    Write a simple LAD file for an independent graph """
-    with open(direct + f'/i{num}.lad', 'w', encoding='utf8') as fil:
-        fil.write(f'{num}\n')
-        fil.write('\n'.join(num * ['0']))
-        fil.write('\n')
