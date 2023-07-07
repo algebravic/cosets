92ced7d2df3735dfdb958036e64136bcd75adb01
Victor S. Miller <victor.miller@sri.com>
2023-07-07 10:20:42 -0700

    Added remove_node_and_neighbors, truncate and new_solve

--- cosets/__init__.py
+++ cosets/__init__.py
@@ -1,6 +1,8 @@
 from .dndata import dn_graph, dn_group, dn_mis_tree
 from .output import write_csv, write_dimacs
 from .maxsat import maxsat_mis
+from .graphs import remove_node_and_neighbors, truncate
+from .greedy import new_solve
 
 __all__ = ['dn_graph',
            'dn_group',
@@ -8,4 +10,7 @@ __all__ = ['dn_graph',
            'write_csv',
            'write_dimacs',
            'maxsat_mis',
+           'remove_node_and_neighbors',
+           'truncate',
+           'new_solve'
            ]
