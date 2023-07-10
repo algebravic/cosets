7aed813e5e4d6ccd59e147a868b77031d9440808
Victor S. Miller <victor.miller@sri.com>
2023-07-10 11:43:26 -0700

    Simple MIP model for Independent Set

---
+++ cosets/mip_model.py
@@ -0,0 +1,24 @@
+"""
+Use the mip package to model independent set
+"""
+from typing import Iterable, Hashable, Tuple, Dict
+import networkx as nx
+from mip import Model, INTEGER, CONTINUOUS, BINARY, xsum
+
+def mip_model(gph: nx.Graph) -> Tuple[Model, Dict[Hashable, int]]:
+    """
+    Use the standard mip_model.
+    """
+    ngph = nx.convert_node_labels_to_integers(gph,
+                                              ordering='sorted')
+    dct = {ind: val for ind, val in enumerate(sorted(gph.nodes))}
+    mvars = {}
+    model = Model()
+    mvars = [model.add_var(name = f'x{node}', var_type=BINARY)
+             for node in ngph.nodes]
+        
+    for node1, node2 in ngph.edges:
+        model += mvars[node1] + mvars[node2] <= 1
+    model.objective = xsum(mvars)
+
+    return model, dct
