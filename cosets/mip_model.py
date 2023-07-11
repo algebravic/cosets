16a81466ced806473075568986fe454dfd5485cf
Victor S. Miller <victorsmiller@gmail.com>
2023-07-10 19:46:57 -0700

    Fixed absence of abs in clause generation

--- cosets/mip_model.py
+++ cosets/mip_model.py
@@ -2,6 +2,7 @@
 Use the mip package to model independent set
 """
 from typing import Iterable, Hashable, Tuple, Dict, List
+from itertools import chain
 import networkx as nx
 from pysat.formula import CNF, WCNF, IDPool
 from mip import Model, INTEGER, CONTINUOUS, BINARY, xsum, MAXIMIZE
@@ -13,12 +14,12 @@ def mip_model(gph: nx.Graph) -> Tuple[Model, Dict[Hashable, int]]:
     """
     ngph = nx.convert_node_labels_to_integers(gph,
                                               ordering='sorted')
-    dct = {ind: val for ind, val in enumerate(sorted(gph.nodes))}
+    dct = dict(enumerate(sorted(gph.nodes)))
     mvars = {}
     model = Model(sense=MAXIMIZE)
     mvars = [model.add_var(name = f'x{node}', var_type=BINARY)
              for node in ngph.nodes]
-        
+
     for node1, node2 in ngph.edges:
         model += mvars[node1] + mvars[node2] <= 1
     model.objective = xsum(mvars)
@@ -27,10 +28,10 @@ def mip_model(gph: nx.Graph) -> Tuple[Model, Dict[Hashable, int]]:
 
 def _get_clause(clause: List[int], variables: List[int]) -> LinExpr:
     """
-    get clause indquality
+    get clause inequality
     """
-    rhs = 1 + len([_ for _ in clause if _ < 0])
-    lhs = xsum([(2 * int(_ > 0) - 1) * variables[_ - 1]
+    rhs = 1 - len([_ for _ in clause if _ < 0])
+    lhs = xsum([(2 * int(_ > 0) - 1) * variables[abs(_) - 1]
                 for _ in clause])
     return lhs >= rhs
 
@@ -46,12 +47,17 @@ def cnf_model(cnf: WCNF) -> Tuple[Model, Dict[int, int]]:
     # First the hard clauses
     for clause in cnf.hard:
         model += _get_clause(clause, variables)
+
     # Now the soft clauses
     objective = []
     for ind, (clause, wgt) in enumerate(zip(cnf.soft, cnf.wght), start=1):
         svar = model.add_var(name='s{ind}', var_type=BINARY)
         variables.append(svar)
-        model += _get_clause([-(top+ind)] + clause, variables)
+        try:
+            model += _get_clause([-(top+ind)] + clause, variables)
+        except IndexError:
+            print(f"top_ind = {top+ind}, len(variables) = {len(variables)}")
+            raise
         objective.append(wgt * svar)
 
     model.objective = xsum(objective)
